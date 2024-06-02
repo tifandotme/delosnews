19b4e3258ff160dac485015983ad7a328b858dee
Tifan <33323177+tifandotme@users.noreply.github.com>
2024-06-02 15:15:40 +0700

    feat: prefetch query

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,25 @@
+import { getArticles } from "@/app/(home)/actions"
 import { Articles } from "@/app/(home)/articles"
+import { PER_PAGE } from "@/lib/constants"
+import { getQueryClient } from "@/lib/react-query"
+import { HydrationBoundary, dehydrate } from "@tanstack/react-query"
 
 export default function Home() {
+  // Prefetch the first page of articles with "emailed" as initial filter
+  const queryClient = getQueryClient()
+  void queryClient.prefetchInfiniteQuery({
+    queryKey: ["articles", null, null],
+    queryFn: async () => {
+      const data = await getArticles("emailed")
+      console.log("data", data.length)
+      return data.slice(0, PER_PAGE)
+    },
+    initialPageParam: 0,
+  })
+
   return (
-    <main className="container mt-[var(--header-height)] pt-5">
-      <h2 className="mb-10 text-center font-serif text-4xl">Explore</h2>
-      <Articles />
-    </main>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Articles className="max-masthead:mt-28" />
+    </HydrationBoundary>
   )
 }
