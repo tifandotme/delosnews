d7781ef7e828067c7df97d6a4c14416fb7377481
Tifan <33323177+tifandotme@users.noreply.github.com>
2024-06-02 15:17:56 +0700

    refactor: make it functional

--- src/app/(home)/actions.ts
+++ src/app/(home)/actions.ts
@@ -1,19 +1,19 @@
 "use server"
 
 import type { FILTERS } from "@/lib/constants"
-import type { MostPopular } from "@/types"
+import type { Response } from "@/types/most-popular"
 
-// const BASE_URL = "https://api.nytimes.com/svc/mostpopular/v2/"
-const BASE_URL = "http://localhost:3100/"
 const FETCH_DELAY = process.env.FETCH_DELAY as string
-// const API_KEY = process.env.NYT_API_KEY as string
 
 /**
  * Server action to fetch articles from the NYT API. Error is handled by React Query hook.
  */
-export async function getArticles(basedOn: (typeof FILTERS)[number]) {
-  // const res = await fetch(`${BASE_URL}${basedOn}/1.json?api-key=${API_KEY}`, {
-  const res = await fetch(BASE_URL + basedOn, {
+export async function getArticles(filter: (typeof FILTERS)[number]) {
+  const url = new URL(
+    `https://api.nytimes.com/svc/mostpopular/v2/${filter}/7.json`,
+  )
+  url.searchParams.set("api-key", process.env.API_KEY as string)
+  const res = await fetch(url, {
     next: {
       revalidate: 3600,
     },
@@ -26,6 +26,6 @@ export async function getArticles(basedOn: (typeof FILTERS)[number]) {
     await new Promise((resolve) => setTimeout(resolve, +FETCH_DELAY))
   }
 
-  const json: MostPopular = await res.json()
+  const json: Response = await res.json()
   return json.results
 }
