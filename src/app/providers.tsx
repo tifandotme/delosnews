17b194e6506be393d2bdc6a76ff57aaf48ed95ab
Tifan <33323177+tifandotme@users.noreply.github.com>
2024-06-04 01:21:28 +0700

    refactor: always lightmode in /lucky

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -5,13 +5,16 @@ import { getQueryClient } from "@/lib/react-query"
 import { QueryClientProvider } from "@tanstack/react-query"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import { ThemeProvider } from "next-themes"
+import { usePathname } from "next/navigation"
 
 export function Providers({ children }: React.PropsWithChildren) {
+  const pathname = usePathname()
   const queryClient = getQueryClient()
 
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider
+        forcedTheme={pathname === "/lucky" ? "light" : undefined}
         disableTransitionOnChange
         enableSystem={false}
         attribute="class"
