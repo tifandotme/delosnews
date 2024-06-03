2f7785cc033ef74d11fc85795868b38dc75f2a60
Tifan <33323177+tifandotme@users.noreply.github.com>
2024-06-04 00:48:13 +0700

    feat: add ownership card above article detail

---
+++ src/app/(home)/[articleId]/ownership-card.tsx
@@ -0,0 +1,135 @@
+"use client"
+
+import { FormatCoins } from "@/components/currencies"
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { Button, buttonVariants } from "@/components/ui/button"
+import { Card } from "@/components/ui/card"
+import { useStore } from "@/lib/stores"
+import { cn } from "@/lib/utils"
+import type { Definitions } from "@/types/article-search"
+import { MoveUpRight } from "lucide-react"
+import Link from "next/link"
+import { useMemo } from "react"
+import { toast } from "sonner"
+
+function calculatePrice(publishedDate: Date) {
+  const MS_IN_A_DAY = 24 * 60 * 60 * 1000
+  const now = new Date()
+  const timeDiff = now.getTime() - publishedDate.getTime()
+  const daysDiff = timeDiff / MS_IN_A_DAY
+
+  if (daysDiff <= 1) {
+    return 50000
+  } else if (daysDiff <= 7) {
+    return 20000
+  } else {
+    return 0
+  }
+}
+
+interface OwnershipCardProps {
+  article: Definitions["Article"]
+}
+
+export function OwnershipCard({ article }: OwnershipCardProps) {
+  const {
+    coins,
+    updateCoins,
+    updateTickets,
+    purchasedArticles,
+    addPurchasedArticles,
+  } = useStore((state) => state)
+
+  const isPurchased = purchasedArticles.some(
+    (purchasedArticle) => purchasedArticle._id === article._id,
+  )
+
+  const price = useMemo(
+    () => calculatePrice(new Date(article.pub_date)),
+    [article.pub_date],
+  )
+
+  return (
+    <Card className="mb-5 flex items-center justify-between gap-2 max-sm:flex-col max-sm:items-start">
+      <div className="w-full">
+        {isPurchased ?
+          <>You have purchased this article.</>
+        : <>
+            Purchase access to this article.
+            <br />
+            You have <FormatCoins amount={coins} /> in your account.
+          </>
+        }
+      </div>
+      <div className="flex w-fit items-center justify-center gap-2">
+        {isPurchased ?
+          <ReadButton externalLink={article.web_url} />
+        : <BuyButton
+            price={price}
+            onConfirm={() => {
+              updateCoins(-coins)
+              addPurchasedArticles(article)
+              if (price >= 50000) {
+                toast("Congratulations! You received 3 Lucky Coins tickets")
+                updateTickets(3)
+              }
+            }}
+          />
+        }
+      </div>
+    </Card>
+  )
+}
+
+function ReadButton({ externalLink }: { externalLink: string }) {
+  return (
+    <Link className={cn(buttonVariants())} href={externalLink} target="_blank">
+      Read
+      <MoveUpRight className="ml-1" size="1.1em" />
+    </Link>
+  )
+}
+
+interface BuyButtonProps {
+  price: number
+  onConfirm: () => void
+}
+
+function BuyButton({ price, onConfirm }: BuyButtonProps) {
+  const { purchasedArticles } = useStore((state) => state)
+
+  const isDisabled = purchasedArticles.length > 5 && price === 0
+
+  return (
+    <>
+      {price === 0 ?
+        <span className="text-sm font-medium uppercase text-muted-foreground">
+          Free
+        </span>
+      : <FormatCoins amount={price} className="text-sm" />}
+      <AlertDialog>
+        <AlertDialogTrigger asChild>
+          <Button disabled={isDisabled}>Buy Article</Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Confirm your purchase</AlertDialogTitle>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Take me back!</AlertDialogCancel>
+            <AlertDialogAction onClick={onConfirm}>Confirm</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
+  )
+}
---
+++ src/components/ui/sonner.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import { useTheme } from "next-themes"
+import { Toaster as Sonner } from "sonner"
+
+type ToasterProps = React.ComponentProps<typeof Sonner>
+
+const Toaster = ({ ...props }: ToasterProps) => {
+  const { theme = "system" } = useTheme()
+
+  return (
+    <Sonner
+      theme={theme as ToasterProps["theme"]}
+      className="toaster group"
+      position="top-center"
+      offset={100}
+      toastOptions={{
+        duration: 5000,
+        classNames: {
+          toast:
+            "rounded-none text-sm group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
+          description: "group-[.toast]:text-muted-foreground",
+          actionButton:
+            "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
+          cancelButton:
+            "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
+        },
+      }}
+      {...props}
+    />
+  )
+}
+
+export { Toaster }
