403e4bfd3251904f3bba383621cfc1eebb268486
Tifan <33323177+tifandotme@users.noreply.github.com>
2024-06-04 06:06:30 +0700

    fix: add missing suspense

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,6 +3,7 @@ import { Articles } from "@/app/(home)/articles"
 import { PER_PAGE } from "@/lib/constants"
 import { getQueryClient } from "@/lib/react-query"
 import { HydrationBoundary, dehydrate } from "@tanstack/react-query"
+import { Suspense } from "react"
 
 export default function Home() {
   // Prefetch the first page of articles with "emailed" as initial filter
@@ -19,7 +20,9 @@ export default function Home() {
   return (
     <div className="container-main min-h-[calc(100vh-var(--masthead-height))] pt-14 max-masthead:mt-28">
       <HydrationBoundary state={dehydrate(queryClient)}>
-        <Articles />
+        <Suspense fallback={null}>
+          <Articles />
+        </Suspense>
       </HydrationBoundary>
     </div>
   )
